36ca179f2c25f688a558af244cd106722c45a12c
公司电脑 <sunyiming537@126.com>
2017-12-04 18:30:36 +0800

    路径生成，支持撤销，反撤销

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,15 +29,23 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.pictureBoxBackGround = new System.Windows.Forms.PictureBox();
             this.pictureBoxForce = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBackGround = new System.Windows.Forms.PictureBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_saveRoute = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackGround)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForce)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -53,31 +61,52 @@
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(1163, 592);
-            this.splitContainer1.SplitterDistance = 925;
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1471, 777);
+            this.splitContainer1.SplitterDistance = 1169;
             this.splitContainer1.TabIndex = 0;
-            // 
-            // pictureBoxBackGround
-            // 
-            this.pictureBoxBackGround.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxBackGround.Location = new System.Drawing.Point(0, 0);
-            this.pictureBoxBackGround.Name = "pictureBoxBackGround";
-            this.pictureBoxBackGround.Size = new System.Drawing.Size(925, 592);
-            this.pictureBoxBackGround.TabIndex = 0;
-            this.pictureBoxBackGround.TabStop = false;
             // 
             // pictureBoxForce
             // 
             this.pictureBoxForce.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBoxForce.Location = new System.Drawing.Point(0, 0);
             this.pictureBoxForce.Name = "pictureBoxForce";
-            this.pictureBoxForce.Size = new System.Drawing.Size(925, 592);
+            this.pictureBoxForce.Size = new System.Drawing.Size(1169, 777);
+            this.pictureBoxForce.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxForce.TabIndex = 1;
             this.pictureBoxForce.TabStop = false;
             this.pictureBoxForce.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBoxForce_MouseClick);
             this.pictureBoxForce.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxForce_MouseMove);
-            this.pictureBoxForce.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.pictureBoxForce_PreviewKeyDown);
+            // 
+            // pictureBoxBackGround
+            // 
+            this.pictureBoxBackGround.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxBackGround.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxBackGround.Name = "pictureBoxBackGround";
+            this.pictureBoxBackGround.Size = new System.Drawing.Size(1169, 777);
+            this.pictureBoxBackGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxBackGround.TabIndex = 0;
+            this.pictureBoxBackGround.TabStop = false;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.button_saveRoute);
+            this.splitContainer2.Panel2.Controls.Add(this.label1);
+            this.splitContainer2.Panel2.Controls.Add(this.label2);
+            this.splitContainer2.Size = new System.Drawing.Size(298, 777);
+            this.splitContainer2.SplitterDistance = 388;
+            this.splitContainer2.TabIndex = 1;
             // 
             // textBox1
             // 
@@ -86,26 +115,66 @@
             this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(234, 592);
-            this.textBox1.TabIndex = 0;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(298, 388);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 20F);
+            this.label1.Location = new System.Drawing.Point(3, 349);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 20F);
+            this.label2.Location = new System.Drawing.Point(105, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // button_saveRoute
+            // 
+            this.button_saveRoute.Location = new System.Drawing.Point(24, 18);
+            this.button_saveRoute.Name = "button_saveRoute";
+            this.button_saveRoute.Size = new System.Drawing.Size(107, 62);
+            this.button_saveRoute.TabIndex = 2;
+            this.button_saveRoute.Text = "保存路径";
+            this.button_saveRoute.UseVisualStyleBackColor = true;
+            this.button_saveRoute.Click += new System.EventHandler(this.button_saveRoute_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1163, 592);
+            this.ClientSize = new System.Drawing.Size(1471, 777);
             this.Controls.Add(this.splitContainer1);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
-            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackGround)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForce)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackGround)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -115,7 +184,11 @@
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.PictureBox pictureBoxForce;
         private System.Windows.Forms.PictureBox pictureBoxBackGround;
+        private System.Windows.Forms.SplitContainer splitContainer2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_saveRoute;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace guandao
 {
@@ -25,14 +20,27 @@ namespace guandao
 
         Size size;
         //已绘制点坐标集合
-        Point[] point;
+        Point[] point;//原始坐标
+        Point[] NewPoint;//新坐标
         //点坐标个数
-        int length = 0;
+        int length = 1;
+        //光标当前位置
+        Point NowPoint;
+
+        //已经撤销的步数
+        int CancelStep = 0;
+
+        //绘图模式
+        bool MapMode = false;
+
+
         //图层初始化
         void Draw_Init()
         {
             size = pictureBoxForce.Size;
             point = new Point[1000];
+            NewPoint = new Point[1000];
+            point[0] = new Point(0, size.Height);
 
             #region 背景图层绘制网格
             Bitmap bmp = new Bitmap(size.Width, size.Height);
@@ -77,12 +85,6 @@ namespace guandao
             int x = e.X;
             int y = e.Y;
 
-            Bitmap bmp = new Bitmap(size.Width, size.Height);
-            Graphics g = Graphics.FromImage(bmp);
-            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            Brush bush = new SolidBrush(Color.Black);//填充的颜色
-
             #region 计算自动归位坐标
             //计算最近坐标点
             int snapx = size.Width / 50;
@@ -100,51 +102,20 @@ namespace guandao
                 y -= yval;
             #endregion
 
+            NowPoint.X = x;
+            NowPoint.Y = y;
 
-            //画定位十字线
-            int snap = size.Width / 30;
-            g.DrawLine(new Pen(Color.DarkCyan, 1), x, y - snap, x, y + snap);//竖线
-            g.DrawLine(new Pen(Color.DarkCyan, 1), x - snap, y, x + snap, y);//横线
-
-            //画定位小交叉线
-            g.DrawLine(new Pen(Color.LightGray, 1), x - snap / 3, y - snap / 3, x + snap / 3, y + snap / 3);
-            g.DrawLine(new Pen(Color.LightGray, 1), x + snap / 3, y - snap / 3, x - snap / 3, y + snap / 3);
-
-            //画已经绘制的线段,并在拐点处画小叉号
-            if (length >= 2)
-            {
-                for (int i = 0; i < length - 1; i++)
-                {
-                    //画已经绘制的线段
-                    g.DrawLine(new Pen(Color.Blue, 3), point[i], point[i + 1]);
-                    //画小叉号
-                    g.DrawLine(new Pen(Color.Gray, 1), point[i].X - snap / 4, point[i].Y - snap / 4, point[i].X + snap / 4, point[i].Y + snap / 4);
-                    g.DrawLine(new Pen(Color.Gray, 1), point[i].X + snap / 4, point[i].Y - snap / 4, point[i].X - snap / 4, point[i].Y + snap / 4);
-                }
-            }
-
-            //画正在绘制的线段
-            if (length >= 1)
-            {
-                g.DrawLine(new Pen(Color.Blue, 3), point[length - 1], new Point(x, y));
-            }
-
-
-
-            //在图片上画圆
-            //g.FillEllipse(bush, x - r, y - r, 2 * r, 2 * r);
-
-            //g.DrawLine(new Pen(Color.Red), 0,1454, withx, heithty);//坐标
-            //g.DrawLine(new Pen(Color.Red, 5), 0, 0, x, y);//坐标
-
-            g.Dispose();
-            pictureBoxForce.Image = bmp;
+            //重新加载界面
+            ReloadUI();
         }
 
         private void pictureBoxForce_MouseClick(object sender, MouseEventArgs e)
         {
             int x = e.X;
             int y = e.Y;
+
+            MapMode = true;
+
             #region 计算自动归位坐标
             //计算最近坐标点
             int snapx = size.Width / 50;
@@ -162,18 +133,280 @@ namespace guandao
                 y -= yval;
             #endregion
 
-            point[length++] = new Point(x, y);
-            textBox1.Text = string.Empty;
-            textBox1.AppendText("point.Length:" + length.ToString() + "\r\n");
-            for (int i = 0; i < length; i++)
+            NowPoint.X = x;
+            NowPoint.Y = y;
+
+            if (e.Button == MouseButtons.Left && e.Clicks == 1)
             {
-                textBox1.AppendText( i.ToString() + " x:" + (point[i].X/snapx).ToString() + ",y:" + (point[i].Y/snapy).ToString() + "\r\n");
+                //单击了鼠标左键,添加点
+                point[length++] = new Point(x, y);
+                NewPoint[length - 1].X = point[length - 1].X / snapx;
+                NewPoint[length - 1].Y = 30 + 1 - point[length - 1].Y / snapy;
+
+                textBox1.Text = string.Empty;
+                textBox1.AppendText("point.Length:" + length.ToString() + "\r\n");
+                for (int i = 0; i < length; i++)
+                {
+                    textBox1.AppendText(i.ToString() + " x:" + NewPoint[i].X.ToString() + ",y:" + (NewPoint[i].Y).ToString() + "\r\n");
+                }
+            }
+            else if (e.Button == MouseButtons.Right && e.Clicks == 1)
+            {
+                //单击了鼠标右键，退出绘图模式
+                MapMode = false;
+                ReloadUI();
+            }
+
+            label1.Text = length.ToString();
+            label2.Text = CancelStep.ToString();
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.Shift && e.KeyCode == Keys.Z)
+            {
+                //恢复撤销
+                if (CancelStep > 1)
+                {
+                    CancelStep--;
+                    length++;
+                    ReloadUI();
+                    label1.Text = length.ToString();
+                    label2.Text = CancelStep.ToString();
+                }
+            }
+            else
+                if (e.KeyCode == Keys.Escape)
+            {
+                //退出绘图模式
+                MapMode = false;
+                ReloadUI();
+            }
+            else if (e.Control && e.KeyCode == Keys.Z)
+            {
+                //撤销
+                if (length > 1)
+                {
+                    length--;
+                    CancelStep++;
+                    //重新加载界面
+                    ReloadUI();
+                    label1.Text = length.ToString();
+                    label2.Text = CancelStep.ToString();
+                }
             }
         }
 
-        private void pictureBoxForce_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        //重新加载界面
+        void ReloadUI()
+        {
+            int x = NowPoint.X;
+            int y = NowPoint.Y;
+            if (MapMode)
+            {
+                Bitmap bmp = new Bitmap(size.Width, size.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                Brush bush = new SolidBrush(Color.Black);//填充的颜色
+
+                //画定位十字线
+                int snap = size.Width / 30;
+                g.DrawLine(new Pen(Color.DarkCyan, 1), x, y - snap, x, y + snap);//竖线
+                g.DrawLine(new Pen(Color.DarkCyan, 1), x - snap, y, x + snap, y);//横线
+
+                //画定位小交叉线
+                g.DrawLine(new Pen(Color.LightGray, 1), x - snap / 3, y - snap / 3, x + snap / 3, y + snap / 3);
+                g.DrawLine(new Pen(Color.LightGray, 1), x + snap / 3, y - snap / 3, x - snap / 3, y + snap / 3);
+
+                //画已经绘制的线段,并在拐点处画小叉号
+                if (length >= 2)
+                {
+                    for (int i = 0; i < length - 1; i++)
+                    {
+                        //画已经绘制的线段
+                        g.DrawLine(new Pen(Color.Blue, 3), point[i], point[i + 1]);
+                        //画小叉号
+                        g.DrawLine(new Pen(Color.Gray, 1), point[i].X - snap / 4, point[i].Y - snap / 4, point[i].X + snap / 4, point[i].Y + snap / 4);
+                        g.DrawLine(new Pen(Color.Gray, 1), point[i].X + snap / 4, point[i].Y - snap / 4, point[i].X - snap / 4, point[i].Y + snap / 4);
+                    }
+                }
+
+                //画正在绘制的线段
+                if (length >= 1)
+                {
+                    g.DrawLine(new Pen(Color.Blue, 3), point[length - 1], new Point(x, y));
+                }
+
+                g.Dispose();
+                pictureBoxForce.Image = bmp;
+            }
+            else
+            {
+                Bitmap bmp = new Bitmap(size.Width, size.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                Brush bush = new SolidBrush(Color.Black);//填充的颜色
+
+
+                //画已经绘制的线段
+                if (length >= 2)
+                {
+                    for (int i = 0; i < length - 1; i++)
+                    {
+                        //画已经绘制的线段
+                        g.DrawLine(new Pen(Color.Blue, 3), point[i], point[i + 1]);
+
+                        //在拐点处画圆
+                        int r = 8;
+                        g.FillEllipse(bush, point[i].X - r, point[i].Y - r, 2 * r, 2 * r);
+                    }
+                }
+                if (length != 0)
+                {
+                    //在拐点处画圆
+                    int r = 8;
+                    int i = length - 1;
+                    g.FillEllipse(bush, point[i].X - r, point[i].Y - r, 2 * r, 2 * r);
+                }
+
+                g.Dispose();
+                pictureBoxForce.Image = bmp;
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
         {
 
+        }
+
+        private void button_saveRoute_Click(object sender, EventArgs e)
+        {
+            if (length > 1)
+            {
+                SaveFileDialog file1 = new SaveFileDialog();
+                file1.Filter = "文本文件|*.xml";
+                if (file1.ShowDialog() == DialogResult.OK)
+                {
+                    XmlTextWriter writer = new XmlTextWriter(file1.FileName, null);
+                    //使用自动缩进便于阅读
+                    writer.Formatting = Formatting.Indented;
+
+                    //写入根元素
+                    writer.WriteStartElement("Flash参数");
+
+                    writer.WriteStartElement("系统参数");
+                    //加入子元素
+                    for (int i = 0; i < 100; i++)
+                    {
+                        writer.WriteStartElement("参数" + i.ToString());
+                        writer.WriteElementString("参数号", i.ToString());
+                        writer.WriteElementString("参数名", "");
+                        if (i == 99)//路径数
+                        {
+                            writer.WriteElementString("当前值", "1");
+                            writer.WriteElementString("设定值", "1");
+                        }
+                        else if(i == 96)//站点包含信息数
+                        {
+                            writer.WriteElementString("当前值", "10");
+                            writer.WriteElementString("设定值", "10");
+                        }
+                        else
+                        {
+                            writer.WriteElementString("当前值", "0");
+                            writer.WriteElementString("设定值", "0");
+                        }
+
+                        writer.WriteElementString("描述", "");
+                        writer.WriteEndElement();
+                    }
+                    writer.WriteEndElement();
+
+
+                    writer.WriteStartElement("路径参数");
+                    //加入子元素
+                    for (int i = 0; i < 1; i++)
+                    {
+                        writer.WriteStartElement("路径" + (i + 1).ToString());
+                        for (int j = 0; j < length; j++)
+                        {
+                            writer.WriteStartElement("站点" + (j + 1).ToString());
+                            //站点信息  
+
+                            writer.WriteStartElement("参数0");
+                            writer.WriteElementString("参数号", "1");
+                            writer.WriteElementString("参数名", " ");
+                            writer.WriteElementString("当前值", "0");
+                            writer.WriteElementString("设定值", "0");
+                            writer.WriteElementString("描述", "X坐标，单位CM");
+                            writer.WriteEndElement();
+
+                            writer.WriteStartElement("参数1");
+                            writer.WriteElementString("参数号", "2");
+                            writer.WriteElementString("参数名", "X");
+                            writer.WriteElementString("当前值", NewPoint[j].X.ToString());
+                            writer.WriteElementString("设定值", NewPoint[j].X.ToString());
+                            writer.WriteElementString("描述", "X坐标，单位CM");
+                            writer.WriteEndElement();
+
+                            writer.WriteStartElement("参数2");
+                            writer.WriteElementString("参数号", "3");
+                            writer.WriteElementString("参数名", "Y");
+                            writer.WriteElementString("当前值", NewPoint[j].Y.ToString());
+                            writer.WriteElementString("设定值", NewPoint[j].Y.ToString());
+                            writer.WriteElementString("描述", "Y坐标，单位CM");
+                            writer.WriteEndElement();
+
+
+                            for (int k = 0; k < 2; k++)
+                            {
+                                writer.WriteStartElement("参数" + (k + 3).ToString());
+                                writer.WriteElementString("参数号", (k + 4).ToString());
+                                writer.WriteElementString("参数名", "未定义");
+                                writer.WriteElementString("当前值", "0");
+                                writer.WriteElementString("设定值", "0");
+                                writer.WriteElementString("描述", "");
+                                writer.WriteEndElement();
+                            }
+
+                            writer.WriteStartElement("参数5");
+                            writer.WriteElementString("参数号", "6");
+                            writer.WriteElementString("参数名", "动作");
+                            writer.WriteElementString("当前值", "0");
+                            writer.WriteElementString("设定值", "0");
+                            writer.WriteElementString("描述", "0:保持 1:停止 2:前进");
+                            writer.WriteEndElement();
+
+                            for (int k = 0; k < 4; k++)
+                            {
+                                writer.WriteStartElement("参数" + (k + 6).ToString());
+                                writer.WriteElementString("参数号", (k + 7).ToString());
+                                writer.WriteElementString("参数名", "未定义");
+                                writer.WriteElementString("当前值", "0");
+                                writer.WriteElementString("设定值", "0");
+                                writer.WriteElementString("描述", "");
+                                writer.WriteEndElement();
+                            }
+
+                            writer.WriteEndElement();
+                        }
+                        writer.WriteEndElement();
+                    }
+
+                    writer.WriteEndElement();
+                    //关闭根元素，并书写结束标签
+                    writer.WriteEndElement();
+                    //将XML写入文件并且关闭XmlTextWriter
+                    writer.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("没有路径可保存到PC！", "提示");
+            }
         }
     }
 }
